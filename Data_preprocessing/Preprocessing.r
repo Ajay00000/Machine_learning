01b5ff24b336fcd993e866a42b6b4a0e149903e1
Ajay00000 <ajay9046@gmail.com>
2020-11-25 20:37:47 +0530

    This is Data Preprocessing Using R

---
+++ Data_preprocessing/Preprocessing.r
@@ -0,0 +1,25 @@
+getwd()
+setwd("/home/aj/Desktop")
+
+data = read.csv("Data.csv")
+data$age = ifelse(is.na(data$age),ave(data$age,FUN = function(x) mean(x,na.rm=TRUE)),data$age)
+data$sallary = ifelse(is.na(data$sallary),ave(data$sallary, FUN = function(x) mean(x,na.rm = TRUE)),data$sallary)
+
+data$country = factor(data$country,levels = c('france','spain','germany'),
+                      labels = c(1,2,3))
+
+data$purchased = factor(data$purchased,levels = c('no','yes'),
+                      labels = c(4,1))
+prcomp(data)
+# spliting data training set test set
+
+set.seed(123)
+smp_size <- floor(0.8 * nrow(data))
+train_ind <- sample(seq_len(nrow(data)), size = smp_size)
+
+train <- data[train_ind, ]
+test <- data[-train_ind, ]
+
+# Feature Scaling
+train[,2:3] = scale(train[,2:3])
+test[,2:3] = scale(test[,2:3])
